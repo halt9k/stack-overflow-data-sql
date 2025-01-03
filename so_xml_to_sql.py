7f9c9eade73740949b2cae7b9fd56a1fa2d24b4a
halt9k <49090368+halt9k@users.noreply.github.com>
2025-01-04 01:03:21 +0300

    ﻿wip: index repair prep

--- so_xml_to_sql.py
+++ so_xml_to_sql.py
@@ -6,54 +6,46 @@ from pathlib import Path
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
-from lxml.etree import XMLParser, parse
-
 
 # create database SO on (name='SO', filename='<custom_path>\test.db')
 DRIVER = "ODBC Driver 17 for SQL Server"
 USERNAME = ""
 PSSWD = ""
-SERVERNAME = "(localdb)\MSSQLLocalDB"
-INSTANCENAME = "\SQLEXPRESS"
+SERVERNAME = r"(localdb)\MSSQLLocalDB"
+INSTANCENAME = r"\SQLEXPRESS"
 DB = "SO"
 TABLE = ""
 
 
-# connection_string = "DRIVER={ODBC Driver 17 for SQL Server};Server=(localdb)\MSSQLLocalDB;Integrated Security=true'"
-# connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-
-connection_string = f"DRIVER={DRIVER};SERVER={SERVERNAME};DATABASE={DB};UID={USERNAME};PWD={PSSWD}"
-connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-engine = create_engine(connection_url, fast_executemany=True, pool_pre_ping=True)
-
-
-def import_xml(xml_file):
-    # parser = XMLParser(huge_tree=True)
-    # tree = parse(xml_file, parser=parser)
-    # root = tree.getroot()
-    #
-    # data = [
-    #     {elem.tag: elem.text for elem in row}
-    #     for row in root.findall('.//row')
-    # ]
-    #
-    # df = pd.DataFrame(data)
-
+def import_xml(engine, xml_file):
     df = pd.read_xml(xml_file, parser='etree')
-
     table_name = Path(xml_file).stem
     df.to_sql(table_name, engine, if_exists="replace", index=False, chunksize=500000)
 
     print(f"Data loaded into table '{table_name}'")
 
 
-with engine.connect() as connection:
-    import_xml("meta.stackexchange.com/Votes.xml")
-    import_xml("meta.stackexchange.com/Tags.xml")
-    import_xml("meta.stackexchange.com/Badges.xml")
-    import_xml("meta.stackexchange.com/Comments.xml")
-    import_xml("meta.stackexchange.com/PostHistory.xml")
-    import_xml("meta.stackexchange.com/PostLinks.xml")
-    import_xml("meta.stackexchange.com/Posts.xml")
-    import_xml("meta.stackexchange.com/Tags.xml")
-    import_xml("meta.stackexchange.com/Users.xml")
+def import_so_xmls(engine):
+    import_xml(engine, "meta.stackexchange.com/Votes.xml")
+    import_xml(engine, "meta.stackexchange.com/Tags.xml")
+    import_xml(engine, "meta.stackexchange.com/Badges.xml")
+    import_xml(engine, "meta.stackexchange.com/Comments.xml")
+    import_xml(engine, "meta.stackexchange.com/PostHistory.xml")
+    import_xml(engine, "meta.stackexchange.com/PostLinks.xml")
+    import_xml(engine, "meta.stackexchange.com/Posts.xml")
+    import_xml(engine, "meta.stackexchange.com/Tags.xml")
+    import_xml(engine, "meta.stackexchange.com/Users.xml")
+
+
+def create_index(engine):
+    pass
+
+
+if __name__ == "__main__":
+    connection_string = f"DRIVER={DRIVER};SERVER={SERVERNAME};DATABASE={DB};UID={USERNAME};PWD={PSSWD}"
+    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+    engine = create_engine(connection_url, fast_executemany=True, pool_pre_ping=True)
+
+    with engine.connect() as connection:
+        # import_so_xmls(engine)
+        create_index(engine)
